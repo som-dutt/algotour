5457283aebf105d48ed6249b101aea167d560b42
som <duttsom89@gmail.com>
2018-10-31 10:58:59 +0530

    Insertion sort impl

---
+++ programmingbit-services/src/main/java/com/programmingbit/algotour/service/Sort.java
@@ -0,0 +1,17 @@
+package com.programmingbit.algotour.service;
+
+/**
+ * @author somdutt
+ *
+ */
+public abstract class Sort {
+
+	public abstract void sort(Integer[] arr);
+
+	public void printArray(Integer[] arr) {
+		for (int x : arr) {
+			System.out.print(x + " ");
+		}
+		System.out.println();
+	}
+}
